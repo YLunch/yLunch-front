260881cd7b76933148ba877f75e79778c012cd95
stephaned <stephane.dupre@indigen.com>
2022-01-12 12:36:33 +0100

    #27 created form style

--- src/app/restaurants/restaurants.module.ts
+++ src/app/restaurants/restaurants.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { RestaurantsRoutingModule } from './restaurants-routing.module';
 import { RegistrationComponent } from './registration/registration.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    RegistrationComponent
-  ],
+  declarations: [RegistrationComponent],
   imports: [
     CommonModule,
-    RestaurantsRoutingModule
-  ]
+    RestaurantsRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
-export class RestaurantsModule { }
+export class RestaurantsModule {}
