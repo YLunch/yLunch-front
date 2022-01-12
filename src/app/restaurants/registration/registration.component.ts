1be9f9d2ed172ed7cd399e2b2ba13b97f7fa73a6
Mickael-DP <m.dallepasqualine@gmail.com>
2022-01-12 14:34:15 +0100

    formbuilder validator

--- src/app/restaurants/registration/registration.component.ts
+++ src/app/restaurants/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validator, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  public registrationForm;
+
+  constructor() { this.registrationForm = new FormBuilder().group({
+    lastName:["", Validators.compose([Validators.required])],
+    firstName:["", Validators.compose([Validators.required])],
+    email:["", Validators.compose([Validators.email, Validators.required])],
+    phone:["", Validators.compose([Validators.required, Validators.pattern('^0[0-9]([0-9]{2}){4}$')])],
+    password:["", Validators.compose([Validators.required])],
+    confirmPassword:["", Validators.compose([Validators.required])]
+  })}
 
   ngOnInit(): void {
   }
 
+  submit() {
+
+    if (this.registrationForm.valid){
+      console.log(this.registrationForm.value);
+      if (this.checkPassword()){
+        console.log('password identique');
+      }
+    }
+  }
+
+  checkPassword(){
+    return this.registrationForm.value.password == this.registrationForm.value.confirmPassword;
+  }
 }
--- src/app/restaurants/restaurants.module.ts
+++ src/app/restaurants/restaurants.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegistrationComponent],
@@ -17,6 +18,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
 })
 export class RestaurantsModule {}
