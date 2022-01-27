4a2fb6079e232ffa1138c784d58b1a4344dcb20a
Mickael-DP <m.dallepasqualine@gmail.com>
2022-01-27 10:10:34 +0100

    custom login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,30 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {HttpClientModule} from "@angular/common/http";
 import {MatButtonModule} from "@angular/material/button";
+import { LoginComponent } from './common/authentificate/login/login.component';
+import { FormComponent } from './common/authentificate/login/form/form.component';
+import {CommonModule} from "@angular/common";
+import {RestaurantsRoutingModule} from "./restaurants/restaurants-routing.module";
+import {MatCardModule} from "@angular/material/card";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatFormFieldModule, MatInputModule, HttpClientModule, MatButtonModule],
+  declarations: [AppComponent, LoginComponent, FormComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    CommonModule,
+    RestaurantsRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
